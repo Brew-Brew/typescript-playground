f915ce2e1e2f99731c51a51e503c3c4d58711f9f
seungkyu2 <zx6658@naver.com>
2019-01-19 17:33:30 +0900

    feat: :sparkles: get api by axios

--- Tutorials/react-by-sample-project/src/api/memberAPI.ts
+++ Tutorials/react-by-sample-project/src/api/memberAPI.ts
@@ -1,45 +1,26 @@
+import Axios, { AxiosResponse } from "axios";
 import { MemberEntity } from "../model/member";
 
-// Sync mock data API, inspired from:
-// https://gist.github.com/coryhouse/fd6232f95f9d601158e4
-class MemberAPI {
-  // Just return a copy of the mock data
-  getAllMembers(): Promise<MemberEntity[]> {
-    const gitHubMembersUrl: string =
-      "https://api.github.com/orgs/lemoncode/members";
+const gitHubURL = "https://api.github.com";
+const gitHubMembersUrl = `${gitHubURL}/orgs/lemoncode/members`;
 
-    return fetch(gitHubMembersUrl)
-      .then(response => this.checkStatus(response))
-      .then(response => this.parseJSON(response))
-      .then(data => this.resolveMembers(data));
-  }
-
-  private checkStatus(response: Response): Promise<Response> {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response);
-    } else {
-      let error = new Error(response.statusText);
-      throw error;
+const getAllMembers = (): Promise<MemberEntity[]> => {
+  const promise: Promise<MemberEntity[]> = new Promise((resolve, reject) => {
+    try {
+      Axios.get<MemberEntity[]>(gitHubMembersUrl).then(response =>
+        resolve(mapMemberListApiToModel(response))
+      );
+    } catch (ex) {
+      reject(ex);
     }
-  }
+  });
 
-  private parseJSON(response: Response): any {
-    return response.json();
-  }
+  return promise;
+};
 
-  private resolveMembers(data: any): Promise<MemberEntity[]> {
-    const members = data.map(gitHubMember => {
-      var member: MemberEntity = {
-        id: gitHubMember.id,
-        login: gitHubMember.login,
-        avatar_url: gitHubMember.avatar_url
-      };
+const mapMemberListApiToModel = ({ data }: AxiosResponse<MemberEntity[]>) =>
+  data.map(gitHubMember => gitHubMember);
 
-      return member;
-    });
-
-    return Promise.resolve(members);
-  }
-}
-
-export const memberAPI = new MemberAPI();
+export const memberAPI = {
+  getAllMembers
+};
